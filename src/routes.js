b6eb0849d4054428027458bd7381b3e6b8f8bc0b
Natthaphon Phanit <nulladivides@protonmail.com>
2017-12-25 00:39:14 +0700

    created new post form

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Route, IndexRoute} from 'react-router';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 import App from './components/app';
 
+const Greeting = () => {
+    return (
+        <div>Hey there!</div>
+    );
+};
+
 export default (
-    <Route path="/" component={App} />
+    <Route path="/" component={App}>
+        <IndexRoute component={PostsIndex} />
+        <Route path="posts/new" component={PostsNew} />
+    </Route>
 );
